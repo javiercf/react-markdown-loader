8130b94b4d8725ab5297fa2804fa6adc2ef293f9
Paul Lange <paul.lange@data-experts.de>
2017-08-22 15:34:13 +0200

    Added basic state control

--- src/build.js
+++ src/build.js
@@ -34,12 +34,25 @@ module.exports = function build(markdown) {
   return `
 ${doImports}
 
-export const attributes = ${JSON.stringify(camelize(frontMatterAttributes))};
-export default function() {
-  return (
-    <div>
-      ${jsx}
-    </div>
-  );
-};`;
+export default class extends React.PureComponent {
+  constructor(props) {
+    super(props)
+
+    this.state = Object.assign({}, this.props, ${JSON.stringify(camelize(frontMatterAttributes))})
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState(nextProps)
+  }
+
+  render () {
+    const state = this.state
+    const setState = this.setState.bind(this)
+    return (
+      <div>
+        ${jsx}
+      </div>
+    )
+  }
+}`;
 };
